bab1d8f67ce2f492796c5ab131e19365ead25cfa
artdata1 <48270726+artdata1@users.noreply.github.com>
2021-11-16 16:37:20 +0700

    update id_to_uuid

--- app/Models/UserBanking.php
+++ app/Models/UserBanking.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use App\Traits\Uuids;
 
 class UserBanking extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Uuids;
 
     protected $fillable = [
         'user_id', 'bank_id', 'bank_account_name', 'bank_account_number', 'is_active', 'status'
--- app/Models/UserPgsoftgame.php
+++ app/Models/UserPgsoftgame.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuids;
 
 class UserPgsoftgame extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuids;
 
     protected $fillable = [
         'user_id', 'player_session', 'operator_player_session'
