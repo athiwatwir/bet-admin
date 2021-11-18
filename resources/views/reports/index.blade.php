8549c06bbecf7f790a47a85fee23ad00f24e4898
Athiwat Wiroonphattanadate <athiwat.wir@gmail.com>
2021-11-18 16:14:38 +0700

    Update index.blade.php

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -17,10 +17,56 @@
 							</select>
 						</div>
 						<div class="col-12 col-md-3 form-group">
-							<input typ="date" name="startdate" class="form-control" />
+							<div class="input-group-over position-realtive z-index-1 bg-white">
+
+								<input type="text" name="startdate" class="form-control bg-transparent datepicker" 
+															data-today-highlight="true" 
+															data-layout-rounded="true" 
+															data-title="" 
+															data-show-weeks="true" 
+															data-today-highlight="true" 
+															data-today-btn="true" 
+															data-autoclose="true" 
+															data-format="MM/DD/YYYY"
+															data-quick-locale='{
+																"days": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+																"daysShort": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+																"daysMin": ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+																"months": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+																"monthsShort": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+																"today": "Today",
+																"clear": "Clear",
+																"titleFormat": "MM yyyy"}'>
+
+								<span class="fi fi-calendar fs--20 ml-4 mr-4 z-index-n1 text-muted"></span>
+							</div>					
+
 						</div>
+
 						<div class="col-12 col-md-3 form-group">
-							<input typ="text" name="enddate" class="form-control" />
+							<div class="input-group-over position-realtive z-index-1 bg-white">
+
+								<input type="text" name="enddate" class="form-control bg-transparent datepicker" 
+															data-today-highlight="true" 
+															data-layout-rounded="true" 
+															data-title="" 
+															data-show-weeks="true" 
+															data-today-highlight="true" 
+															data-today-btn="true" 
+															data-autoclose="true" 
+															data-format="MM/DD/YYYY"
+															data-quick-locale='{
+																"days": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+																"daysShort": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+																"daysMin": ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+																"months": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+																"monthsShort": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+																"today": "Today",
+																"clear": "Clear",
+																"titleFormat": "MM yyyy"}'>
+
+								<span class="fi fi-calendar fs--20 ml-4 mr-4 z-index-n1 text-muted"></span>
+							</div>
 						</div>
 						<div class="col-12 col-md-2 form-group">
 							<button class="btn btn-primary btn-block" type="submit">GO!</button>
