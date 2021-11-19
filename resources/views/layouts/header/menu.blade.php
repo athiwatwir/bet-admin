78e53776242158a9332df30d2e982c9c76061e28
artdata1 <48270726+artdata1@users.noreply.github.com>
2021-11-19 12:28:50 +0700

    Update menu.blade.php

--- resources/views/layouts/header/menu.blade.php
+++ resources/views/layouts/header/menu.blade.php
@@ -93,54 +93,6 @@
         </div>
     </li>
 
-    <!-- Games -->
-    <li class="nav-item dropdown">
-        <a href="#" class="nav-link nav-link-caret-hide dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="mainNavFeatures">
-            <i class="fi fi-shield-ok float-start m-0"></i>
-            <span>เกม</span> 
-        </a>
-
-        <div class="dropdown-menu dropdown-menu-hover w--300 p-0 border border-light overflow-hidden" aria-labelledby="mainNavFeatures">
-            <div class="row no-gutters bg-gradient-secondary">
-
-                <!-- REMOVE BAR IF NOT NEEDED -->
-                <div class="col-2 d-none d-lg-block"><!-- empty -->
-                    <!-- text-rotate-90 , text-rotate-180 -->
-                    <p class="h6 font-weight-medium mb-0 text-white text-rotate-180 text-center position-absolute d-middle">
-                        รายการเกม
-                    </p>
-                </div>
-
-                <div class="col bg-white overflow-hidden">
-                    <ul class="mx-0 px-0 my-2">
-                        <li class="dropdown-item text-wrap">
-                            <a href="{{ url('/games/groups') }}" class="dropdown-link text-dark transition-hover-start p-3 line-height-1">
-                                <i class="fi fi-users float-start fs--25 mt--n2"></i>
-                                <span class="h5-xs d-block fs--18">กลุ่มเกม</span>
-                                <span class="fs--11 text-muted text-uppercase">
-                                    กลุ่มเกมทั้งหมด
-                                </span>
-                            </a>
-                        </li>
-
-                        <li class="dropdown-item text-wrap">
-                            <a href="{{ url('/games') }}" class="dropdown-link text-dark transition-hover-start p-3 line-height-1">
-                                <i class="fi fi-users float-start fs--25 mt--n2"></i>
-                                <span class="h5-xs d-block fs--18">รายการเกม</span>
-                                <span class="fs--11 text-muted text-uppercase">
-                                    รายการเกมทั้งหมด
-                                </span>
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </li>
-    <!-- Football -->
-    
-
     <!-- SETTING -->
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="mainNavHome">
@@ -170,15 +122,15 @@
                 </ul>
             </li>
         </ul>
-
-        
     </li>
+
     <li class="nav-item">
         <a href="{{ route('reports') }}" class="nav-link nav-link-caret-hide" id="">
             <i class="fas fa-chart-pie float-start m-0 mt-1"></i>
             <span>รายงาน</span>
         </a>
     </li>
+
     <li class="nav-item dropdown">
         <a href="#" class="nav-link nav-link-caret-hide dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="mainNavFeatures">
             <i class="fi fi-shield-ok float-start m-0"></i>
@@ -233,6 +185,53 @@
             </div>
         </div>
     </li>
+
+    <!-- Games -->
+    <li class="nav-item dropdown">
+        <a href="#" class="nav-link nav-link-caret-hide dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="mainNavFeatures">
+            <i class="fi fi-shield-ok float-start m-0"></i>
+            <span>เกม</span> 
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-hover w--300 p-0 border border-light overflow-hidden" aria-labelledby="mainNavFeatures">
+            <div class="row no-gutters bg-gradient-secondary">
+
+                <!-- REMOVE BAR IF NOT NEEDED -->
+                <div class="col-2 d-none d-lg-block"><!-- empty -->
+                    <!-- text-rotate-90 , text-rotate-180 -->
+                    <p class="h6 font-weight-medium mb-0 text-white text-rotate-180 text-center position-absolute d-middle">
+                        รายการเกม
+                    </p>
+                </div>
+
+                <div class="col bg-white overflow-hidden">
+                    <ul class="mx-0 px-0 my-2">
+                        <li class="dropdown-item text-wrap">
+                            <a href="{{ url('/games/groups') }}" class="dropdown-link text-dark transition-hover-start p-3 line-height-1">
+                                <i class="fi fi-users float-start fs--25 mt--n2"></i>
+                                <span class="h5-xs d-block fs--18">กลุ่มเกม</span>
+                                <span class="fs--11 text-muted text-uppercase">
+                                    กลุ่มเกมทั้งหมด
+                                </span>
+                            </a>
+                        </li>
+
+                        <li class="dropdown-item text-wrap">
+                            <a href="{{ url('/games') }}" class="dropdown-link text-dark transition-hover-start p-3 line-height-1">
+                                <i class="fi fi-users float-start fs--25 mt--n2"></i>
+                                <span class="h5-xs d-block fs--18">รายการเกม</span>
+                                <span class="fs--11 text-muted text-uppercase">
+                                    รายการเกมทั้งหมด
+                                </span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </li>
+    <!-- END Game -->
 
 </ul>
 </div>
